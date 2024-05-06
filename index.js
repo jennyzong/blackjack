7115fc9e4194d24ee4385e6b40c299364c2e60cb
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-05 20:08:10 -0400

    blackjack
    -create JS file, display some new variables and use textContent to display some variables

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard + 4
+let secondCard = 4
+let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
-    hasBlackJack = true
-} else {
-    console.log("You're out of the game! 😭")
-    isAlive = false
+function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
 }
 
-console.log(isAlive)
+
+function newCard() {
+    let card = 6
+    sum += card
+    startGame()
+}
