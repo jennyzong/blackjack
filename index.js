f25ab6e40b53eee0c0ca73dbf5af762a0bff2b2d
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-06 20:47:09 -0400

    blackjack
    -solve the problem with arrays in JS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 10
 let secondCard = 4
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -9,7 +10,11 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -27,5 +32,6 @@ function startGame() {
 function newCard() {
     let card = 6
     sum += card
-    startGame()
+    // Push the card to the cards array
+    renderGame()
 }
