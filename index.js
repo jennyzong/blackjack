2dbe53dc7768419e9aa45463e69b38ff3732cfc5
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-07 20:42:39 -0400

    blackjack
    -use math object to solve some problems in JS

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 10
-let secondCard = 4
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
@@ -9,13 +9,22 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+
+function getRandomCard() {
+    // if 1     -> return 11
+    // if 11-13 -> return 10
+    return Math.floor( Math.random()*13 ) + 1
+}
+
 function startGame() {
     renderGame()
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
-    // Create a for loop that renders out all the cards instead of just two
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
     
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
@@ -32,10 +41,8 @@ function renderGame() {
 
 
 function newCard() {
-    let card = 6
+    let card = getRandomCard()
     sum += card
-    // Push the card to the cards array
     cards.push(card)
-    console.log(cards)
     renderGame()
 }
